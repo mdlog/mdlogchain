e3667bfc1993a3cea005f3e9bebe33c357d37b6d
mdlog <adiadi2411@gmail.com>
2022-09-30 00:58:14 +0800

    Update themeConfig.js

--- themeConfig.js
+++ themeConfig.js
@@ -9,9 +9,9 @@ export const $themeBreakpoints = {}
 // APP CONFIG
 export const $themeConfig = {
   app: {
-    appName: 'PING.PUB', // Will update name in navigation menu (Branding)
+    appName: 'MDLOG', // Will update name in navigation menu (Branding)
     // eslint-disable-next-line global-require
-    appLogoImage: '/logo.svg', // Will update logo in navigation menu (Branding)
+    appLogoImage: '/2e7ffc43ac9141fdb8d9400000fb8b35.png', // Will update logo in navigation menu (Branding)
   },
   layout: {
     isRTL: false,
